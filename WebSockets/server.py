8927a788b29419b9f65200e758430d49095a32de
gurtejtalwar <gurtejtalwar@gmail.com>
2024-02-08 18:22:01 +0530

    added: server.py in WebSockets

---
+++ WebSockets/server.py
@@ -0,0 +1,18 @@
+import asyncio
+import websockets
+
+async def hello(websockets):
+    name = await websockets.recv()
+    print(f"Server Received: {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websockets.send(greeting)
+    print(f"Server Sent: {greeting}")
+
+async def main():
+    async with websockets.serve(hello, "localhost", 8765):
+        await asyncio.Future()  # run forever
+    
+if __name__ == "__main__":
+    asyncio.run(main())
