dec07886947081618ba7c33c0a6474648fb1479e
gurtejtalwar <gurtejtalwar@gmail.com>
2024-02-09 15:33:16 +0530

    completed: Figma Loader

--- LangChain/figma-app/main.py
+++ LangChain/figma-app/main.py
@@ -8,20 +8,24 @@ from langchain.prompts.chat import (
 )
 from langchain_community.document_loaders.figma import FigmaFileLoader
 from langchain_openai import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+import chromadb
+from dotenv import load_dotenv
+
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 figma_loader = FigmaFileLoader(
-    os.environ.get("ACCESS_TOKEN"),
-    os.environ.get("NODE_IDS"),
-    os.environ.get("FILE_KEY"),
+    os.getenv("ACCESS_TOKEN"),
+    os.getenv("NODE_IDS"),
+    os.getenv("FILE_KEY"), 
 )
 
-# see https://python.langchain.com/en/latest/modules/data_connection/getting_started.html for more details
-index = VectorstoreIndexCreator().from_loaders([figma_loader])
+index = VectorstoreIndexCreator(embedding=OpenAIEmbeddings()).from_loaders([figma_loader])
 figma_doc_retriever = index.vectorstore.as_retriever()
 
 def generate_code(human_input):
     # I have no idea if the Jon Carmack thing makes for better code. YMMV.
-    # See https://python.langchain.com/en/latest/modules/models/chat/getting_started.html for chat info
     system_prompt_template = """You are expert coder Jon Carmack. Use the provided design context to create idiomatic HTML/CSS code as possible based on the user request.
     Everything must be inline in one file and your response must be directly renderable by the browser.
     Figma file nodes and metadata: {context}"""
@@ -37,6 +41,7 @@ def generate_code(human_input):
     gpt_4 = ChatOpenAI(temperature=0.02, model_name="gpt-4")
     # Use the retriever's 'get_relevant_documents' method if needed to filter down longer docs
     relevant_nodes = figma_doc_retriever.get_relevant_documents(human_input)
+    print(f"Relevant nodes: {relevant_nodes}")
     conversation = [system_message_prompt, human_message_prompt]
     chat_prompt = ChatPromptTemplate.from_messages(conversation)
     response = gpt_4(
@@ -45,3 +50,7 @@ def generate_code(human_input):
         ).to_messages()
     )
     return response
+
+response = generate_code("Meal Calculator Page") 
+
+print (response)
